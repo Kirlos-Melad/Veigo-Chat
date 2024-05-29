c20b4d8a6989e9b85f0c46688bfd9f251c2dca22
Kirlos Melad <kirlosmelad2000@gmail.com>
2024-05-29 07:47:33 +0300

    Remove cors config

--- backend/gateway/source/index.ts
+++ backend/gateway/source/index.ts
@@ -23,13 +23,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(
-	cors({
-		origin: "*",
-		// methods: ["POST"],
-		// allowedHeaders: ["Content-Type", "Authorization"],
-	}),
-);
+app.use(cors());
 
 app.use("ws", WebSocketProxy);
 
