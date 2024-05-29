a4d4869f8711bdd8646af8737430a90688d436bc
Kirlos Melad <kirlosmelad2000@gmail.com>
2024-05-29 07:47:50 +0300

    Authorize ws connections

--- backend/gateway/source/websocket/WebSocketProxy.ts
+++ backend/gateway/source/websocket/WebSocketProxy.ts
@@ -2,6 +2,7 @@ import { createProxyMiddleware } from "http-proxy-middleware";
 
 import Environments from "../configurations/Environments";
 import Logger from "../utilities/Logger";
+import Authorize from "../utilities/Authorize";
 
 const WebSocketProxy = createProxyMiddleware({
 	target: Environments.RT_SERVICE_CONNECTION,
@@ -11,8 +12,24 @@ const WebSocketProxy = createProxyMiddleware({
 	changeOrigin: true,
 	ws: true,
 	on: {
-		proxyReqWs: (proxyRequest, request, socket, options, head) => {
-			Logger.information("Got a websocket connection");
+		proxyReqWs: async (proxyRequest, request, socket, options, head) => {
+			try {
+				await Authorize(request.headers["sec-websocket-protocol"]);
+			} catch (error: any) {
+				const responseHeaders = [
+					"HTTP/1.1 401 Unauthorized",
+					"Content-Type: text/plain",
+					"Content-Length: " + Buffer.byteLength(error.message),
+					"Connection: close",
+				];
+				const response =
+					responseHeaders.join("\r\n") + "\r\n\r\n" + error.message;
+				socket.write(response, (err) => {
+					if (err) Logger.warning(err);
+					socket.destroy();
+				});
+				return;
+			}
 		},
 	},
 });
