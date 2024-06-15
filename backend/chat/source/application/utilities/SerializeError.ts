93a2e9e71594609f5c12bdb940810211be7e2451
Kirlos Melad <kirlosmelad2000@gmail.com>
2024-06-15 19:54:41 +0300

    Add DB error code

--- backend/chat/source/application/utilities/SerializeError.ts
+++ backend/chat/source/application/utilities/SerializeError.ts
@@ -1,6 +1,14 @@
 import { ZodError } from "zod";
 import pg from "pg";
 
+const ERROR_NAME = {
+	INTERNAL_SERVER_ERROR: "Internal Server Error",
+};
+
+const DB_ERROR_CODE = {
+	SYNTAX_ERROR: "42601",
+};
+
 class ErrorSerializer {
 	private mError: any;
 	private mSerializedError?: Record<string, any>;
@@ -12,6 +20,12 @@ class ErrorSerializer {
 	private DatabaseErrorInstanceSerializer(): Record<string, any> {
 		const error = this.mError as pg.DatabaseError;
 
+		if (error.code === DB_ERROR_CODE.SYNTAX_ERROR) {
+			return {
+				name: ERROR_NAME.INTERNAL_SERVER_ERROR,
+				message: "An internal error occurred. Please try again later.",
+			};
+		}
 		// TODO: Implement serialization for DatabaseError
 		return error;
 	}
