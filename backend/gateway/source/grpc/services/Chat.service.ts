cb9db898bd50907ab483305289b8fdaf7b638cd8
Kirlos Melad <kirlosmelad2000@gmail.com>
2024-06-15 18:58:01 +0300

    Continue/Fix new protos migration

--- backend/gateway/source/grpc/services/Chat.service.ts
+++ backend/gateway/source/grpc/services/Chat.service.ts
@@ -1,22 +1,22 @@
 import { ProfileClient } from "@source/types/generated/protos/chat/ProfilePackage/Profile";
 import { RoomClient } from "@source/types/generated/protos/chat/RoomPackage/Room";
-import { UserRoomClient } from "@source/types/generated/protos/chat/UserRoomPackage/UserRoom";
 import { MessageClient } from "@source/types/generated/protos/chat/MessagePackage/Message";
 import Environments from "@source/configurations/Environments";
 import GRPCServiceManager from "../GRPCServiceManager";
+import { MemberRoomClient } from "@root/source/types/generated/protos/chat/MemberRoomPackage/MemberRoom";
 
 const ChatService = new GRPCServiceManager<{
 	Profile: ProfileClient;
 	Room: RoomClient;
-	UserRoom: UserRoomClient;
+	MemberRoom: MemberRoomClient;
 	Message: MessageClient;
 }>(
 	"Chat",
 	{
 		Profile: "source/types/generated/protos/chat/definitions/Profile.proto",
 		Room: "source/types/generated/protos/chat/definitions/Room.proto",
-		UserRoom:
-			"source/types/generated/protos/chat/definitions/UserRoom.proto",
+		MemberRoom:
+			"source/types/generated/protos/chat/definitions/MemberRoom.proto",
 		Message: "source/types/generated/protos/chat/definitions/Message.proto",
 	},
 	Environments.CHAT_SERVICE_CONNECTION,
