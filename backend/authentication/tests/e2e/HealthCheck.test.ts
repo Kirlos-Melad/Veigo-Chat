f22cd7b6ad471192b34e48a97971f3f3d1be83fd
Kirlos Melad <kirlosmelad2000@gmail.com>
2024-07-01 17:35:32 +0300

    Add End-To-End tests for authentication service

---
+++ backend/authentication/tests/e2e/HealthCheck.test.ts
@@ -0,0 +1,28 @@
+import { expect } from "chai";
+import CreateGrpcClient from "./utilities/CreateGrpcClient";
+import { HealthCheckClient } from "@source/types/generated/protos/health_check/HealthCheck";
+
+describe("Health Check Service test", () => {
+	let client: HealthCheckClient;
+
+	before((done) => {
+		client = CreateGrpcClient<HealthCheckClient>(
+			"source/types/generated/protos/definitions/health_check.proto",
+			"health_check",
+			"HealthCheck",
+		);
+
+		client.waitForReady(Date.now() + 1000, (error) => done(error));
+	});
+
+	it("Should be serving", () => {
+		client.Check({}, (error, response) => {
+			expect(error).to.be.null;
+			expect(response).to.have.property("status").equal(1);
+		});
+	});
+
+	after(() => {
+		client.close();
+	});
+});
---
+++ backend/authentication/tests/e2e/Login.test.ts
@@ -0,0 +1,150 @@
+import { expect } from "chai";
+import { faker } from "@faker-js/faker";
+
+import CreateGrpcClient from "./utilities/CreateGrpcClient";
+import { AuthenticationClient } from "@source/types/generated/protos/authentication/Authentication";
+
+describe("Profile Service Test", () => {
+	let client: AuthenticationClient;
+	const user: {
+		id?: string;
+		clientId?: string;
+
+		account: {
+			email: string;
+			isEmailVerified?: boolean;
+
+			password: string;
+
+			phone?: string;
+			isPhoneVerified?: boolean;
+		};
+
+		token?: {
+			access: string;
+			refresh: string;
+		};
+	} = {
+		clientId: faker.string.uuid(),
+		account: {
+			email: faker.internet.email(),
+			password: "Password@123",
+		},
+	};
+
+	before((done) => {
+		client = CreateGrpcClient<AuthenticationClient>(
+			"source/types/generated/protos/definitions/authentication.proto",
+			"authentication",
+			"Authentication",
+		);
+
+		client.waitForReady(Date.now() + 1000, (error) => done(error));
+	});
+
+	after(() => {
+		client.close();
+	});
+
+	afterEach("Check if access token is valid", async (done) => {
+		client.ValidateAccessToken(
+			{
+				token: user.token!.access,
+			},
+			(error, response) => {
+				expect(error).to.be.null;
+				expect(response).not.to.be.undefined;
+				expect(response).to.be.empty("object");
+				done();
+			},
+		);
+	});
+
+	it("Should sign up successfully", async (done) => {
+		client.SignUp(
+			{
+				clientId: user.clientId,
+				email: user.account.email,
+				password: user.account.password,
+			},
+			(error, response) => {
+				expect(error).to.be.null;
+				expect(response).not.to.be.undefined;
+
+				// Assert Account Properties
+				expect(response).to.have.property("account");
+				expect(response!.account).to.have.property("id");
+				expect(response!.account)
+					.to.have.property("email")
+					.equal(user.account.email);
+
+				// Assert Token Properties
+				expect(response).to.have.property("token");
+				expect(response!.token).to.have.property("access");
+				expect(response!.token).to.have.property("refresh");
+
+				// Set Current User
+				user.id = response!.account!.id;
+				user.token = {
+					access: response!.token!.access!,
+					refresh: response!.token!.refresh!,
+				};
+
+				done();
+			},
+		);
+	});
+
+	it("Should sign in successfully", async (done) => {
+		client.signIn(
+			{
+				clientId: user.clientId,
+				email: user.account.email,
+				password: user.account.password,
+			},
+			(error, response) => {
+				expect(error).to.be.null;
+				expect(response).not.to.be.undefined;
+
+				// Assert Account Properties
+				expect(response).to.have.property("account");
+				expect(response!.account).to.have.property("id").equal(user.id);
+				expect(response!.account)
+					.to.have.property("email")
+					.equal(user.account!.email);
+				// Assert Token Properties
+				expect(response).to.have.property("token");
+				expect(response!.token).to.have.property("access");
+				expect(response!.token).to.have.property("refresh");
+				// Update Token
+				user.token = {
+					access: response!.token!.access!,
+					refresh: response!.token!.refresh!,
+				};
+
+				done();
+			},
+		);
+	});
+
+	it("Should refresh token successfully", async (done) => {
+		client.RefreshToken(
+			{ token: user.token!.refresh },
+			(error, response) => {
+				expect(error).to.be.null;
+				expect(response).not.to.be.undefined;
+
+				// Assert Token Properties
+				expect(response).to.have.property("access");
+				expect(response).to.have.property("refresh");
+				// Update Token
+				user.token = {
+					access: response!.access!,
+					refresh: response!.refresh!,
+				};
+
+				done();
+			},
+		);
+	});
+});
