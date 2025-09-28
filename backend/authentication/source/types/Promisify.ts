eb6f08e17ed35605ce49894f11056107ce2e9ce8
VeiGo <52179817+Kirlos-Melad@users.noreply.github.com>
2025-09-28 09:03:25 +0300

    chore: clean old data
    Signed-off-by: VeiGo <52179817+Kirlos-Melad@users.noreply.github.com>

--- backend/authentication/source/types/Promisify.ts
+++
@@ -1,3 +0,0 @@
-type Promisify<F extends (...args: any[]) => any> = (
-	...args: Parameters<F>
-) => Promise<ReturnType<F>>;
--- backend/authentication/source/types/nodejs.d.ts
+++
@@ -1,3 +0,0 @@
-namespace NodeJS {
-	interface ProcessEnv extends Record<string, never> {}
-}
